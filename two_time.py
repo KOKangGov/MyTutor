80e91680206d87ac9e8917367724c560a001bab8
KOKangGov <kang705@hotmail.com>
2026-06-12 17:21:48 +0900

    빠진거...

--- two_time.py
+++ two_time.py
@@ -8,3 +8,12 @@ def two_times(numberList):
 
 result = two_times([1, 2, 3, 4])
 print(result)
+
+# map함수를 이용하여 
+def two_times(x):
+    return x*2
+
+list(map(two_times, [1, 2, 3, 4]))
+
+# lambda 함수를 사용하면
+list(map(lambda a: a*2, [1, 2, 3, 4]))
